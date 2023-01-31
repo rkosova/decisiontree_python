6d736d55d424c37dc766444fa8cb1a958f1bcf5b
Ron Kosova <ron.kosova@gmail.com>
2023-01-31 16:12:14 +0200

    Renamed main.py to decisiontree.py

---
+++ decisiontree.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+ 
+def gini(classDistribution: dict[str, float]) -> float:
+    """ Returns the Gini impurity of set
+
+    Arguments:
+    classDistribution: dictionary that associates class name and it's rational distribution within the set
+    """
+    sumSquared: float = 0
+
+    for _class in classDistribution.keys():
+        sumSquared += (classDistribution[_class] ** 2)
+
+    return 1 - sumSquared
+
+
+def getClassDistribution(labels: pd.Series) -> dict[str, float]:
+    """ Returns a dictionary with the unique labels as keys and their rational distribution as the value
+    
+    Arguments:
+    labels: pandas Series, label column from data
+    """
+    classDistribution: dict[str, float] = {}
+    uniqueClassCount: pd.Series = labels.value_counts()
+
+    for i in range(len(uniqueClassCount)):
+        classDistribution.update({
+            str(uniqueClassCount.index[i]): uniqueClassCount[uniqueClassCount.index[i]] / len(labels)
+        })
+    
+    return classDistribution
+
