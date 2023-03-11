9e04f3a7dec03ff9cbb3cbbd3b9844ba4b4bc689
Ron Kosova <ron.kosova@gmail.com>
2023-03-11 17:46:17 +0200

    Data splitting method

--- src/decisiontree/model.py
+++ src/decisiontree/model.py
@@ -1,15 +1,37 @@
 import pandas as pd
 
 class Tree:
-    def __init__(self, data: pd.DataFrame, maxDepth: int = 35) -> None:
+    def __init__(self, data: pd.DataFrame, maxDepth: int = 35, minLabels: int = 10, maxImpurity: float = 0.1) -> None:
+
+        #data
         self.data = data
         self.classDistribution = getClassDistribution(self.data)
 
-        # maxImpurity: float = 0 add to function that determines whether a node is a leaf or not 
+        # hyperparameters
+        self.maxDepth = maxDepth
+        self.minLabels = minLabels
+        self.maxImpurity = maxImpurity
+
+        split = self.findSplit(self.data)
+        leftData, rightData = self.splitData(split, self.data)
+
+        print(leftData.head(), rightData.head())
 
 
-    def findSplit(self, minLabels: float = 0) -> dict:
-        features = self.data.columns[:-1]
+
+    def train(self, data, depth = 0):
+        dataImpurity = gini(getClassDistribution(data))
+
+        if depth == self.maxDepth or dataImpurity <= self.maxImpurity:
+            return Node(None, None, None, data.iloc[:, -1].mode()[0])
+        
+        split = self.findSplit(data)
+
+        leftData, rightData = self.splitData(split, data)
+
+
+    def findSplit(self, data) -> dict:
+        features = data.columns[:-1]
 
         giniSmallest = {
                 "left": {"gini": 1, "feature": None, "value": None, "labels": 0},
@@ -18,16 +40,16 @@ class Tree:
 
         for feature in features:
             
-            uniqueFeatureValues = self.data[feature].unique()
+            uniqueFeatureValues = data[feature].unique()
             uniqueFeatureValues.sort()
 
             for i in range(1, len(uniqueFeatureValues) - 1):
 
                  # values of labels of the data points with feature values to the left of the i-th unique feature value
-                labelsLeft = self.data[self.data[feature] <= uniqueFeatureValues[i]].iloc[:, -1]
+                labelsLeft = data[data[feature] <= uniqueFeatureValues[i]].iloc[:, -1]
 
                 # values of labels of the data points with feature values to the right of the i-th unique feature value
-                labelsRight = self.data[self.data[feature] >= uniqueFeatureValues[i]].iloc[:, -1]
+                labelsRight = data[data[feature] >= uniqueFeatureValues[i]].iloc[:, -1]
 
 
                 # gini values of the i-th unique feature value sliced labels sets 
@@ -36,7 +58,7 @@ class Tree:
 
 
                 if currentGiniLeft < giniSmallest["left"]["gini"]:
-                    if labelsLeft.count() >= minLabels: # labelsLeft.count() >= giniSmallest["left"]["labels"]: find way to implement
+                    if labelsLeft.count() >= self.minLabels: # labelsLeft.count() >= giniSmallest["left"]["labels"]: find way to implement
                         giniSmallest["left"]["gini"] = currentGiniLeft
                         giniSmallest["left"]["feature"] = feature
                         giniSmallest["left"]["value"] = uniqueFeatureValues[i]
@@ -44,22 +66,37 @@ class Tree:
 
 
                 if currentGiniRight < giniSmallest["right"]["gini"]:
-                    if labelsRight.count() >= minLabels: # and labelsRight.count() >= giniSmallest["right"]["labels"]: find way to implement
+                    if labelsRight.count() >= self.minLabels: # and labelsRight.count() >= giniSmallest["right"]["labels"]: find way to implement
                         giniSmallest["right"]["gini"] = currentGiniRight
                         giniSmallest["right"]["feature"] = feature
                         giniSmallest["right"]["value"] = uniqueFeatureValues[i]
                         giniSmallest["right"]["labels"] = labelsRight.count()
 
+   
+        if giniSmallest["left"]["gini"] < giniSmallest["right"]["gini"]:
+            return {"left": giniSmallest["left"]}
+        else:
+            return {"right": giniSmallest["right"]}
 
-        for i in range(2):
-            if giniSmallest["left"]["gini"] < giniSmallest["right"]["gini"]:
-                return {"left": giniSmallest["left"]}
-            else:
-                return {"right": giniSmallest["right"]}
-                
+
+    def splitData(self, split, data):
+        
+        if list(split.keys())[0] == "left":
+            leftData = data[data[split["left"]["feature"]] <= split["left"]["value"]]
+            rightData = data[data[split["left"]["feature"]] > split["left"]["value"]]
+        else:
+            leftData = data[data[split["right"]["feature"]] < split["right"]["value"]]
+            rightData = data[data[split["right"]["feature"]] >= split["right"]["value"]]
+
+        return leftData, rightData
+           
+
 class Node:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, left, right, feature, value) -> None:
+        self.left = left
+        self.right = right
+        self.feature = feature
+        self.value = value
         
 
 def gini(classDistribution: dict[str, float]) -> float:
